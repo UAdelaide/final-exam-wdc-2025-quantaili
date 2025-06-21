c3f0e41d04e9e576feae34d44a8dbfbbd84bafb4
nu yoah <nuyoah@nudeMacBook-Pro.local>
2025-06-21 16:41:38 +0930

    done

--- part2/app.js
+++ part2/app.js
@@ -11,9 +11,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogRoutes = require('./routes/dogRoutes');
+const walkRequestRoutes = require('./routes/walkRequestRoutes');
+const walkerRoutes = require('./routes/walkerRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/dogs', dogRoutes);
+app.use('/api/walkrequests', walkRequestRoutes);
+app.use('/api/walkers', walkerRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
